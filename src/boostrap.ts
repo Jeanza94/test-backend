7787a2662737d43214dd2c16154261d53a63ac4f
Jean Zapata <jeanza94@hotmail.com>
2024-02-24 21:32:40 -0500

    dotenv config

---
+++ src/boostrap.ts
@@ -0,0 +1,26 @@
+import express, { Router } from 'express';
+import { AppService } from "./domain/services/appService";
+import { AppExpress } from "./infrastructure/app-express/app";
+import { ProductRouter } from "./infrastructure/app-express/routes/productRouter";
+import { ProductController } from './infrastructure/app-express/controllers/productControllers';
+import { ProductRepositoryImpl } from './infrastructure/repositories/productRepositoryImpl';
+import { FreeMarketDatasource } from './infrastructure/datasources/freeMarketDatasource';
+import { AxiosApiAdapter } from './infrastructure/adapters/axiosApiAdapter';
+import { AppMiddlewares } from './infrastructure/app-express/middlewares/appMiddleware';
+import { AppRouter } from './infrastructure/app-express/routes/appRouter';
+import { envs } from '../config/envs';
+
+export const boostrap = () => {
+  const expressApp = express()
+  const http = new AxiosApiAdapter()
+  const datasource = new FreeMarketDatasource(http)
+  const productRepository = new ProductRepositoryImpl(datasource)
+  const productController = new ProductController(productRepository)
+  const router = Router()
+  const productRouter = new ProductRouter(router, productController)
+  const appRouter = new AppRouter(productRouter)
+  const appMiddlewares = new AppMiddlewares()
+  const app: AppService = new AppExpress(expressApp, appMiddlewares,appRouter)
+
+  app.listen(envs.PORT)
+}
